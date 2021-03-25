e916e5842dd9f8d408db71550ba8d13e08dc8898
Rk-Thakur <tranjan638@gmail.com>
2021-03-25 11:55:10 +0545

    changes is login and registerid

--- admin/login.php
+++ admin/login.php
@@ -35,40 +35,37 @@ if(isset($_POST['submit'])){
 
 </head>
 <body>
-    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method ="post">
-    
-    <div class="p-2 w-1/2">
-        <div class="relative">
-            <label for="name" class="leading-7 text-sm text-gray-600">Username</label>
-            <input type="text"  name="uname" class="w-20 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-        </div>
-    </div> 
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method ="post">
+<div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+    <div class="mb-4">
+      <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username" name="uname">
+    </div>
     <?php
     if(isset($errUsername)){
         echo $errUsername;
     }
     ?>
-    
-
-    
-    <div class="p-2 w-1/2">
-        <div class="relative">
-            <label for="name" class="leading-7 text-sm text-gray-600">Password</label>
-            <input type="password"  name="password" class="w-20 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-        </div>
-    </div> 
-    
+    <div class="mb-6">
+      <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************" name="password">
+    </div>
     <?php
     if(isset($errPassword)){
         echo $errPassword;
     }
     ?>
-    <br> <br>
-    <input type="checkbox" name="remember" > Remember
-    <br>
-    <br>
-    <input type="submit"  value="Login" name="submit" class=" flex-wrap mx-auto text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg"> 
-
-    </form>
+    <div class="flex items-center justify-between">
+      
+      <input type="submit" name="submit" class="bg-black hover:bg-red-dark text-white font-bold py-2 px-4 rounded" value="Sign In ">
+      <a href="registerid.php" class="bg-black hover:bg-red-dark text-white font-bold py-2 px-4 rounded"> Register</a>
+    </div>
+</div>
+</form>
 </body>
 </html>
+
--- admin/registerid.php
+++ admin/registerid.php
@@ -36,24 +36,30 @@ if(isset($_POST['submit'])){
 
 </head>
 <body>
-    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method ="post">
-    
-    <div class="p-2 w-1/2">
-        <div class="relative">
-            <label for="name" class="leading-7 text-sm text-gray-600">Username</label>
-            <input type="text" id="name" name="uname" class="w-20 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-        </div>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method ="post">
+<div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+    <div class="mb-4">
+      <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username" name="uname">
     </div>
-    <div class="p-2 w-1/2">
-        <div class="relative">
-            <label for="name" class="leading-7 text-sm text-gray-600">Password</label>
-            <input type="password"  name="password" class="w-20 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-        </div>
-    </div>
-
-
-    <input type="submit"  value="Register"name="submit" class=" mx-auto text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg"> 
     
-    </form>
+    <div class="mb-6">
+      <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************" name="password">
+    </div>
+    
+    <div class="flex items-center justify-between">
+      
+      <input type="submit" name="submit" class="bg-black hover:bg-red-dark text-white font-bold py-2 px-4 rounded" value="Register">
+      <a href="login.php" class="bg-black hover:bg-red-dark text-white font-bold py-2 px-4 rounded"> Login</a>
+    </div>
+</div>
+</form>
 </body>
 </html>
+
+

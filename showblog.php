693270b4275d1fb7bd3686f29bb6bed979aa94a7
Rk-Thakur <tranjan638@gmail.com>
2021-03-31 16:58:30 +0545

    showblog.php

--- showblog.php
+++ showblog.php
@@ -54,7 +54,7 @@
       </nav>
       
 
-      <a href="register.html" class="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg  ">Member!</a>
+      <a href="register.php" class="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg  ">Member!</a>
     </div>
   </header>
 

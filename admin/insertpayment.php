20ff15e6b07ecb1bfebfc912518544e40669b7ba
Rk-Thakur <tranjan638@gmail.com>
2021-04-09 18:37:32 +0545

    completed

--- admin/insertpayment.php
+++ admin/insertpayment.php
@@ -36,32 +36,57 @@ else{
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BLOG</title>
+    <title>Payment</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
 </head>
 <body>
-    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST" enctype="multipart/form-data">
-    Collection of Three months: <br> <br>
-    <br>
-    Member_Id:
-    <input type="number" name="id"> <br> <br>
-    Date:
-    <input type="date" name="date"> <br> <br>
-    Total:
-    <input type="number" name="amt"> <br> <br>
-    Due:
-    <input type="number" name="due"> <br> <br>
     
-    <input type="submit" name="submit" Value="submit">
-    </form> <br> <br>
-</body>
-</html>
 
-<table>
-<tr>
-    <th>Id</th>
-    <th>Member_Id</th>
-    <th>Name</th>
+
+
+<div class="flex items-center h-screen w-full bg-gray-300">
+
+  <div class="w-full bg-white rounded  p-8 m-4 md:max-w-sm ">
+
+    <h1 class=" w-full text-center text-grey-darkest mb-6">Collection For 3 Months:</h1>
+
+    <form class="" action="<?php echo $_SERVER['PHP_SELF']?>" method="POST" >
+      <div class="flex flex-col mb-4 md:w-full">
+        <label class="mb-2 uppercase  font-bold text-lg text-grey-darkest" >Member_Id:</label>
+        <input class="border py-2 px-3 text-grey-darkest md:mr-2" type="number" name="id" >
+      </div>
+      <div class="flex flex-col mb-4 md:w-full">
+        <label class="mb-2 uppercase font-bold text-lg text-grey-darkest md:ml-2" >Date:</label>
+        <input class="border py-2 px-3 text-grey-darkest md:ml-2" type="date" name="date" >
+      </div>
+      <div class="flex flex-col mb-4 md:w-full">
+        <label class="mb-2 uppercase font-bold text-lg text-grey-darkest">Total</label>
+        <input class="border py-2 px-3 text-grey-darkest" type="number" name="amt" >
+      </div>
+      <div class="flex flex-col mb-4 md:w-full">
+        <label class="mb-2 uppercase font-bold text-lg text-grey-darkest">Due</label>
+        <input class="border py-2 px-3 text-grey-darkest" type="number" name="due" >
+      </div>
+
+      <button class="block   text-white bg-gray-900   uppercase  mx-auto p-4 rounded" type="submit" name="submit">
+      Upload
+      </button>
+      
+    </form>
+    
+  </div>
+  <div class="info " >
+    <p class=" text-5xl text-center font-bold m-5">Payment Details</p>
+  <table class="rounded-lg m-5 w-5/6 mx-auto bg-white text-gray-800">
+<thead>
+<tr class="text-left border-b-4 border-gray-900">
+    <th class="px-4 py-3 text-center">Id</th>
+    <th class="px-4 py-3 text-center">Member_Id</th>
+    <th class="px-4 py-3 text-center">Name</th>
 </tr>
+</thead>
+<tbody>
 <tr>
 <?php
                   include_once("config.php");
@@ -74,9 +99,9 @@ else{
                   if($result){                 
                   while($row=mysqli_fetch_assoc($result)){?>
                   <tr>
-                <td class="border border-green-600 "><?php echo $count; ?></td>
-                <td class="border border-green-600 "><?php echo $row['id']; ?></td>
-                <td class="border border-green-600 "><?php echo $row["name"]; ?></td>
+                <td class="px-4 py-3 text-center"><?php echo $count; ?></td>
+                <td class="px-4 py-3 text-center"><?php echo $row['id']; ?></td>
+                <td class="px-4 py-3 text-center"><?php echo $row["name"]; ?></td>
                     
                 </tr>
                 <?php $count++;?>
@@ -86,8 +111,13 @@ else{
                   ?>
 </tr>
 </table>
+  </div>
 
 
+
+  
+</body>
+</html>
 
 
 
